c8d2daf1d681c83a1af1849461a82befb53f0e77
bopaki <btebalo@gmail.com>
2023-06-11 01:34:56 +0200

    fixed the bug, API is working

--- labour/urls.py
+++ labour/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/permits', include('permits.urls')),
+    path('api/', include('permits.urls')),
 ]
--- permits/urls.py
+++ permits/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import LongTermPermitListCreateView, LongTermPermitDetailView
 
 urlpatterns = [
-    path('long-term-permits/', LongTermPermitDetailView.as_view(), name='longtermpermit-list'),
+    path('long-term-permits/', LongTermPermitListCreateView.as_view(), name='longtermpermit-list'),
     path('long-term-permits/<int:pk>/', LongTermPermitDetailView.as_view(), name='longtermpermit-detail'),
 ]
