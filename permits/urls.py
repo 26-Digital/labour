5c294e87e1e6a52fb652f8481f70fa8145794fe6
bopaki <btebalo@gmail.com>
2023-06-11 01:06:20 +0200

    updated urls

---
+++ permits/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import LongTermPermitListCreateView, LongTermPermitDetailView
+
+urlpatterns = [
+    path('long-term-permits/', LongTermPermitDetailView.as_view(), name='longtermpermit-list'),
+    path('long-term-permits/<int:pk>/', LongTermPermitDetailView.as_view(), name='longtermpermit-detail'),
+]
