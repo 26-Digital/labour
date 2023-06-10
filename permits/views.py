ce5bb7ed8435db9585eb23f4a41d4d4aa6617017
bopaki <btebalo@gmail.com>
2023-06-11 00:26:50 +0200

    updated views

--- permits/views.py
+++ permits/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import LongTermPermit
+from .serializers import LongTermPermitSerializer
 
-# Create your views here.
+class LongTermPermitListCreateView(generics.ListCreateAPIView):
+    queryset = LongTermPermit.objects.all()
+    serializer_class = LongTermPermitSerializer
+
+class LongTermPermitDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = LongTermPermit.objects.all()
+    serializer_class = LongTermPermitSerializer
