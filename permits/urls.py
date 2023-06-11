343b726687fc43a84bf85c5e2a5401deb5421cd2
bopaki <btebalo@gmail.com>
2023-06-11 15:39:05 +0200

    added compute years of residence url

--- permits/urls.py
+++ permits/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LongTermPermitListCreateView, LongTermPermitDetailView, LongTermWorkPermitApprovalView
+from .views import LongTermPermitListCreateView, LongTermPermitDetailView, LongTermWorkPermitApprovalView, YearsOfResidenceView
 
 urlpatterns = [
     path('long-term-permits/', LongTermPermitListCreateView.as_view(), name='longtermpermit-list'),
     path('long-term-permits/<int:pk>/', LongTermPermitDetailView.as_view(), name='longtermpermit-detail'),
     path('long-term-permits/<path:permit_number>/approval/', LongTermWorkPermitApprovalView.as_view(), name='longtermpermit-approval'),
+    path('residence/<str:passport_number>/', YearsOfResidenceView.as_view(), name='residence-view')
 ]
